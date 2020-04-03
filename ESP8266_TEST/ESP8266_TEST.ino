75660e8d946b1434293410648339eabe6f2079e7
4497cv <ie707560@iteso.mx>
2020-04-03 01:20:19 -0700

    broker is set

--- ESP8266_TEST/ESP8266_TEST.ino
+++ ESP8266_TEST/ESP8266_TEST.ino
@@ -1,5 +1,5 @@
 /*
-    \file:   iotcrpmonitor.cpp
+    \file:   iotcrpmonitor.ino
     \brief:  This is source file for the implementation of the IoT crop monitor
     \author: César Villarreal @4497cv
     \date:   01/04/2020
@@ -15,15 +15,21 @@
  * Define Constants
  ****************************************/
 #define TOKEN          "BBFF-Sl9GbpiTrf6pVe93LsjEFK1tMr5ZCq" // Your Ubidots TOKEN
-#define WIFINAME       "ARRIS-AC62" //Your SSID
-#define WIFIPASS       "F3DA7DAD49EFE77B" // Your Wifi Pass
+#define WIFINAME_CASA  "ARRIS-AC62" //Your SSID
+#define WIFIPASS_CASA  "F3DA7DAD49EFE77B" // Your Wifi Pass
+#define WIFINAME       "The Coolest Pool Wifi"
+#define WIFIPASS       "2WC466403315"
 #define DEVICE_LABEL   "esp8266"
 #define PWROUT_VALUE   62
-#define TEMP_DELAY     60  //1 min delay
+#define TEMP_DELAY     120 //2 min delay
 #define LUM_DELAY      300 //5 min delay
-#define HUM_DELAY      120 //2 min delay
+#define HUM_DELAY      240 //4 min delay
 
-static float temp_read;
+static uint8_t master_switch_st;
+static float temp_delay;
+static float lum_delay;
+static float sm_delay;
+static char* sw_topic = "/v1.6/devices/control/master-switch/lv";
 
 Ubidots client(TOKEN);
 
@@ -34,11 +40,14 @@ void temperature_isr()
     float sensor_val;
     cd4051be_setChannel(CH_0);
     sensor_val = LM35DZ_get_current_temperature(celcius);
-    if((temp_limit_low < sensor_val) && (temp_limit_high > sensor_val))
+    
+    if((temp_limit_low  <  sensor_val) &&
+       (temp_limit_high >  sensor_val))
     {
         client.add("lm35", sensor_val);
         client.ubidotsPublish("node-mcu");  
     }
+    
     client.loop();
 }
 
@@ -61,42 +70,33 @@ void soil_humidity_isr()
     client.ubidotsPublish("node-mcu");
     client.loop();
 }
-/****************************************
- * Auxiliar Functions
- ****************************************/
-uint8_t verifyPowerActive(void)
-{
-  uint8_t flag;
-  float sensor_val;
-  
-  sensor_val = cd4051be_ChannelRead(CH_1);
-  sensor_val = (sensor_val*100.0)/1024.0;
-  sensor_val = (uint8_t) sensor_val;
-  
-  if(PWROUT_VALUE == sensor_val)
-  {
-    flag = 0;
-    Serial.print("Power is not active\n");
-  }
-  else
-  {
-    flag = 1;
-  }
-
-  return flag;
-}
 
 void callback(char* topic, byte* payload, unsigned int length)
 {
-  Serial.print("Message arrived [");
+  int i;
   Serial.print(topic);
-  Serial.print("] ");
-  for (int i=0;i<length;i++)
-  {
-    Serial.print((char)payload[i]);
-  }
 
+  for (i = 0; i < length ; i++) Serial.print((char)payload[i]);
   Serial.println();
+
+  if(topic == sw_topic)
+  {
+      Serial.printf("MS payload [0] = %f\n", payload[0]);
+      master_switch_st = payload[0];
+  }
+//    break;
+//    case "temp_delay":
+//      Serial.printf("temp payload [0] = %f\n", payload[0]);
+//      //ticker0.attach(TEMP_DELAY, temperature_isr);
+//    break;
+//    case "lum_delay":
+//      Serial.printf("lum payload [0] = %f\n", payload[0]);
+//      //ticker1.attach(TEMP_DELAY, temperature_isr);
+//    break;
+//    case "sm_delay":
+//       Serial.printf("temp payload [0] = %f\n", payload[0]);
+//      //ticker2.attach(TEMP_DELAY, temperature_isr);
+//    break; 
 }
 
 /****************************************
@@ -113,15 +113,16 @@ void setup()
 
   /* establish serial communication */
   Serial.begin(115200);
+  client.ubidotsSetBroker("industrial.api.ubidots.com");
   /* activate debugging messages on console */
   client.setDebug(true); // Pass a true or false bool value to activate debug messages
   /* establish wifi communication */
-  client.wifiConnection(WIFINAME, WIFIPASS);
+  client.wifiConnection(WIFINAME_CASA, WIFIPASS_CASA);
   /* set message callback function */
   client.begin(callback);
   /* subscribe to a device variable */
-  client.ubidotsSubscribe("node-mcu","temperature"); //Insert the dataSource and Variable's Labels
-  //client.ubidotsPublish("switch");
+  client.ubidotsSubscribe("control","master-switch"); //Insert the dataSource and Variable's Labels
+  
   ticker0.attach(TEMP_DELAY, temperature_isr);
   ticker1.attach(LUM_DELAY,  luminosity_isr);
   ticker2.attach(HUM_DELAY,  soil_humidity_isr);
@@ -132,5 +133,9 @@ void loop()
   if(!client.connected())
   {
       client.reconnect();
+      client.ubidotsSubscribe("control","master-switch"); //Insert the dataSource and Variable's Labels
   }
+
+  client.loop();
+  delay(300);
 }
